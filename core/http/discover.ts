4e31b4b03a630f446d2dcc5677374e1b673ef5a8
Saif Ali Khan <saffellikhan@gmail.com>
2026-04-27 17:30:36 +0500

    refactor: router.ts and discover.ts

--- core/http/discover.ts
+++ core/http/discover.ts
@@ -1,5 +1,5 @@
 import { loadHooks } from "./hooks.ts";
-import { loadRoutes } from "./routes.ts";
+import { findRouter } from "./routes.ts";
 import { TMethod, TRouteExecutor } from "./router.ts";
 
 export const discover = async (req: Request): Promise<TRouteExecutor> => {
@@ -10,31 +10,27 @@ export const discover = async (req: Request): Promise<TRouteExecutor> => {
   );
   const [namespace, ...endpointParts] = pathnameParts;
 
-  const { fallback, module } = await loadRoutes(
+  const { fallback, module } = await findRouter(
     `./**/*.ts`,
     namespace + ".ts",
   );
 
   const router = module.default;
 
-  if (typeof router?.route === "function") {
-    const resolvedEndpoint = fallback
-      ? `/${pathnameParts.join("/") ?? ""}`
-      : `/${endpointParts.join("/") ?? ""}`;
+  const resolvedEndpoint = fallback
+    ? `/${pathnameParts.join("/") ?? ""}`
+    : `/${endpointParts.join("/") ?? ""}`;
 
-    const exec = router.route(
-      req.method.toLowerCase() as TMethod,
-      resolvedEndpoint,
-    );
+  const exec = router.route(
+    req.method.toLowerCase() as TMethod,
+    resolvedEndpoint,
+  );
 
-    return async (req: Request) =>
-      typeof exec === "function"
-        ? await exec(
-          req,
-          ...(await loadHooks(`./**/*.ts`)),
-        )
-        : new Response("Not found", { status: 404 });
-  }
-
-  throw new Error(`Not a valid router at ${url.pathname}`);
+  return async (req: Request) =>
+    typeof exec === "function"
+      ? await exec(
+        req,
+        ...(await loadHooks(`./**/*.ts`)),
+      )
+      : new Response("Not found", { status: 404 });
 };
--- core/http/routes.ts
+++ core/http/routes.ts
@@ -4,8 +4,9 @@ import { join } from "@std/path/join";
 import { toFileUrl } from "@std/path/to-file-url";
 import { cache } from "@/core/utils/cache.ts";
 import { listPlugins } from "@/core/lib/listPlugins.ts";
+import { Router } from "@/core/http/router.ts";
 
-export const loadRoutes = cache(async (
+export const findRouter = cache(async (
   globPattern: string,
   target: string,
 ) => {
@@ -20,9 +21,13 @@ export const loadRoutes = cache(async (
     })
   ) {
     if (target === basename(entry.path)) {
-      return {
-        module: await import(toFileUrl(entry.path).href),
-      };
+      const module = await import(toFileUrl(entry.path).href);
+
+      if (typeof module.default?.route === "function") {
+        return {
+          module,
+        };
+      }
     }
   }
 
@@ -37,9 +42,13 @@ export const loadRoutes = cache(async (
       })
     ) {
       if (target === basename(entry.path)) {
-        return {
-          module: await import(toFileUrl(entry.path).href),
-        };
+        const module = await import(toFileUrl(entry.path).href);
+
+        if (typeof module.default?.route === "function") {
+          return {
+            module,
+          };
+        }
       }
     }
   }
@@ -48,4 +57,43 @@ export const loadRoutes = cache(async (
     fallback: true,
     module: await import(toFileUrl(join(routesRoot, "index.ts")).href),
   };
+}, Infinity);
+
+export const loadRouters = cache(async (globPattern: string) => {
+  const routesDir = "./routes";
+  const routesRoot = join(Deno.cwd(), routesDir);
+
+  const routers = [];
+
+  for await (
+    const entry of expandGlob(globPattern, {
+      includeDirs: false,
+      globstar: true,
+      root: routesRoot,
+    })
+  ) {
+    const module = await import(toFileUrl(entry.path).href);
+
+    if (typeof module.default?.route === "function") {
+      routers.push(module as Router);
+    }
+  }
+
+  const plugins = await listPlugins();
+
+  for (const plugin of plugins) {
+    for await (
+      const entry of expandGlob(globPattern, {
+        includeDirs: false,
+        globstar: true,
+        root: join(plugin, routesDir),
+      })
+    ) {
+      const module = await import(toFileUrl(entry.path).href);
+
+      if (typeof module.default?.route === "function") {
+        routers.push(module as Router);
+      }
+    }
+  }
 }, Infinity);
