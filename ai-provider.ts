42a214325eca423031d0cb0ed30199c819604b8f
Saif Ali Khan <saffellikhan@gmail.com>
2026-09-12 19:41:09 +0500

    chore: fix minor stuff

--- ai-provider.ts
+++ ai-provider.ts
@@ -1,27 +1,14 @@
-// import { createAnthropic } from "@ai-sdk/anthropic";
-import { createGoogle } from "@ai-sdk/google";
 import { createOpenAI } from "@ai-sdk/openai";
 import { ModelProvider } from "@/core/agent/types.ts";
 import { Env } from "@/core/utils/env.ts";
-
-// const anthropic = createAnthropic({
-//   apiKey: Env.getSync("ANTHROPIC_API_KEY"),
-// });
-
-const google = createGoogle({
-  apiKey: Env.getSync("GOOGLE_AI_API_KEY"),
-});
 
 const openai = createOpenAI({
   apiKey: Env.getSync("OPENAI_API_KEY"),
 });
 
 export default {
-  // architect: anthropic("claude-opus-4-5"),
-  // engineer: anthropic("claude-sonnet-5"),
-  // helper: anthropic("claude-sonnet-5"),
   architect: openai("gpt-6-astra"),
-  engineer: openai("gpt-5.6-luna"),
+  engineer: openai("gpt-5.6-sol"),
   helper: openai("gpt-5.6-luna"),
-  embedder: google.embedding("gemini-embedding-001"),
+  embedder: openai.embedding("text-embedding-3-large"),
 } satisfies ModelProvider;
